cda6a27307cc167211d24b8c9aa6f586c896c66b
Long Pham <longpham@crepeagu.stud.uregina.ca>
2022-10-24 15:10:43 -0600

    using iterations method

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,13 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    finalSum = 0;
+    if (!Number.isInteger(num1) || !Number.isInteger(num2)) return `ERROR`;
+    if (num1 < 0 || num2 < 0) return 'ERROR';
+    largeNumber = num1>num2? num1: num2;
+    smallNumber = num1 + num2 - largeNumber;
+    for (i = smallNumber; i <= largeNumber; i++){
+        finalSum += i;
+    }
+    return finalSum;
 };
 
 // Do not edit below this line
