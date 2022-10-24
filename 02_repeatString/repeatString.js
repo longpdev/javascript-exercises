8fa4acf2fbf73bb512a3863cc39833cffd7f7540
Long Pham <longpham@crepeagu.stud.uregina.ca>
2022-10-24 09:12:33 -0600

    complete exercise 01 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(string, n) {
+    newString ="";
+    if (n < 0) {
+        return `ERROR`;
+    }
+    while(n--)  newString += string;
+    return newString;
 };
 
 // Do not edit below this line
