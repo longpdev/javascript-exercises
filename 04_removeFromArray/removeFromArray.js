ca930abc6e7e2904913c57b988a442a441c39127
Long Pham <longpham@crepeagu.stud.uregina.ca>
2022-10-24 13:00:56 -0600

    complete with filter funcition

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(...args) {
+    const array = args[0];
+     newArray = [];
+    newArray = array.filter(item => !args.includes(item));
+    //filter prototype
+    // item is an element in array, filter through the array 
+    // with the condition: !args.includes(item)
+    return newArray; 
 };
 
 // Do not edit below this line
