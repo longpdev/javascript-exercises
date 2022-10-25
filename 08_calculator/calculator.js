ce834820e4cc4723ac80772427ba310495be1e5d
Long Pham <longpham@Longs-Air.hitronhub.home>
2022-10-24 20:24:20 -0600

    calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,42 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  result = 0;
+  if (array.length == 0)
+  return 0;
+	for (const value of array){
+    result += value;
+  }
+  return result;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  result = 1;
+	for (const value of array){
+    result *= value;
+    if (value == 0)
+    return 0;
+  }
+  return result;
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return Math.pow(a,b);
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  if (n == 0 || n == 1)
+   return 1;
+  result = 1;
+	for ( i=1; i<=n; i++)
+   result *= i;
+  return result;
 };
 
 // Do not edit below this line
